35f36893033cab3f5fe0bc478ef7890f5528eeda
YS19999 <117980405+YS19999@users.noreply.github.com>
2023-10-25 09:49:10 +0800

    Update factory.py

--- PaAT/classifier/factory.py
+++ PaAT/classifier/factory.py
@@ -1,22 +1,15 @@
 import torch
-from embedding.wordebd import WORDEBD
 from embedding.cxtebd import CXTEBD
 from classifier.protoAttn import ProtoAttn
-from classifier.protoAttn1 import ProtoAttn1
-from classifier.PCN import PCN
 
 from dataset.utils import tprint
 
-
 def get_classifier(vocab, args):
     tprint("Building classifier: {}".format(args.classifier))
-
-    # ebd = WORDEBD(vocab, args)
+    
     ebd = CXTEBD(args, return_seq=True)
 
-    # model = ProtoAttn(ebd, args)
-    model = ProtoAttn1(ebd, args)
-    # model = PCN(ebd, args)
+    model = ProtoAttn(ebd, args)
 
     if args.snapshot != '':
         # load pretrained models
