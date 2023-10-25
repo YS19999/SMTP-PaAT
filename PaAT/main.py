e720e029211d1585605ac9df500b01709c18a626
YS19999 <117980405+YS19999@users.noreply.github.com>
2023-10-25 09:45:34 +0800

    Update main.py

--- PaAT/main.py
+++ PaAT/main.py
@@ -1,1 +1,243 @@
+import json
+import os
+import sys
+import pickle
+import signal
+import argparse
+import traceback
+
+import torch
+import numpy as np
+
+import embedding.factory as ebd
+import classifier.factory as clf
+import dataset.loader as loader
+import train.factory as train_utils
+from tools.visual_featuespace import main_feature
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+            description="prototypical-aware attention for few-shot intent detection")
+
+    # data configuration
+    parser.add_argument("--data_path", type=str,
+                        default="data/banking77.csv",
+                        help="path to dataset")
+    parser.add_argument("--dataset", type=str, default="banking77",
+                        help="name of the dataset. "
+                        "Options: [20newsgroup, amazon, huffpost, "
+                        "reuters, rcv1, fewrel]")
+  
+    parser.add_argument("--n_train_class", type=int, default=25,
+                        help="number of meta-train classes")
+    parser.add_argument("--n_val_class", type=int, default=25,
+                        help="number of meta-val classes")
+    parser.add_argument("--n_test_class", type=int, default=27,
+                        help="number of meta-test classes")
+
+    # load bert embeddings for sent-level datasets (optional)
+    parser.add_argument("--n_workers", type=int, default=10,
+                        help="Num. of cores used for loading data. Set this "
+                        "to zero if you want to use all the cpus.")
+    parser.add_argument("--bert", default=True, action="store_true",
+                        help=("set true if use bert embeddings "
+                              "(only available for sent-level datasets: "
+                              "huffpost, fewrel"))
+    parser.add_argument("--bert_cache_dir", default="pretrainedModel/bert-base-uncased", type=str,
+                        help=("path to the cache_dir of transformers"))
+    parser.add_argument("--pretrained_bert", default="pretrainedModel/bert-base-uncased", type=str,
+                        help=("path to the pre-trained bert embeddings."))
+
+    # task configuration
+    parser.add_argument("--way", type=int, default=5,
+                        help="#classes for each task")
+    parser.add_argument("--shot", type=int, default=1,
+                        help="#support examples for each class for each task")
+    parser.add_argument("--query", type=int, default=25,
+                        help="#query examples for each class for each task")
+
+    # train/test configuration
+    parser.add_argument("--train_epochs", type=int, default=50,
+                        help="max num of training epochs")
+    parser.add_argument("--train_episodes", type=int, default=100,
+                        help="#tasks sampled during each training epoch")
+    parser.add_argument("--val_episodes", type=int, default=100,
+                        help="#asks sampled during each validation epoch")
+    parser.add_argument("--test_episodes", type=int, default=1000,
+                        help="#tasks sampled during each testing epoch")
+
+    # settings for finetuning baseline
+    parser.add_argument("--finetune_loss_type", type=str, default="softmax",
+                        help="type of loss for finetune top layer"
+                        "options: [softmax, dist]")
+    parser.add_argument("--finetune_maxepochs", type=int, default=5000,
+                        help="number epochs to finetune each task for (inner loop)")
+    parser.add_argument("--finetune_episodes", type=int, default=10,
+                        help="number tasks to finetune for (outer loop)")
+    parser.add_argument("--finetune_split", default=0.8, type=float,
+                        help="percent of train data to allocate for val"
+                             "when mode is finetune")
+
+    ""
+    # model options
+    parser.add_argument("--embedding", type=str, default="avg",
+                        help=("document embedding method. Options: "
+                              "[avg, lstmatt, meta, oracle, cnn]"))
+    parser.add_argument("--classifier", type=str, default="proto",
+                        help=("classifier. Options: [nn, proto, r2d2, mlp, routing]"))
+    parser.add_argument("--auxiliary", type=str, nargs="*", default=[],
+                        help=("auxiliary embeddings (used for fewrel). "
+                              "Options: [pos, ent]"))
+
+    parser.add_argument("--wv_path", type=str,
+                        default='',
+                        help="path to word vector cache")
+    parser.add_argument("--word_vector", type=str, default='../module/wiki.en.vec',
+                        help=("Name of pretrained word embeddings."))
+    parser.add_argument("--finetune_ebd", action="store_true", default=True,
+                        help=("Finetune embedding during meta-training"))
+
+    # cnn configuration
+    parser.add_argument("--cnn_num_filters", type=int, default=50,
+                        help="Num of filters per filter size [default: 50]")
+    parser.add_argument("--cnn_filter_sizes", type=int, nargs="+",
+                        default=[3, 4, 5],
+                        help="Filter sizes [default: 3]")
+
+    # nn configuration
+    parser.add_argument("--nn_distance", type=str, default="l2",
+                        help=("distance for nearest neighbour. "
+                              "Options: l2, cos [default: l2]"))
+
+    # proto configuration
+    parser.add_argument("--proto_hidden", nargs="+", type=int,
+                        default=[300, 300],
+                        help=("hidden dimension of the proto-net"))
+
+    # maml configuration
+    parser.add_argument("--maml", action="store_true", default=False,
+                        help=("Use maml or not. "
+                        "Note: maml has to be used with classifier=mlp"))
+    parser.add_argument("--mlp_hidden", nargs="+", type=int, default=[768, 10],
+                        help=("hidden dimension of the proto-net"))
+    parser.add_argument("--maml_innersteps", type=int, default=10)
+    parser.add_argument("--maml_batchsize", type=int, default=10)
+    parser.add_argument("--maml_stepsize", type=float, default=1e-1)
+    parser.add_argument("--maml_firstorder", action="store_true", default=False,
+                        help="truncate higher order gradient")
+
+    # lrd2 configuration
+    parser.add_argument("--lrd2_num_iters", type=int, default=5,
+                        help=("num of Newton steps for LRD2"))
+
+    # induction networks configuration
+    parser.add_argument("--induct_rnn_dim", type=int, default=128,
+                        help=("Uni LSTM dim of induction network's encoder"))
+    parser.add_argument("--induct_hidden_dim", type=int, default=100,
+                        help=("tensor layer dim of induction network's relation"))
+    parser.add_argument("--induct_iter", type=int, default=3,
+                        help=("num of routings"))
+    parser.add_argument("--induct_att_dim", type=int, default=64,
+                        help=("attention projection dim of induction network"))
+
+    # aux ebd configuration (for fewrel)
+    parser.add_argument("--pos_ebd_dim", type=int, default=5,
+                        help="Size of position embedding")
+    parser.add_argument("--pos_max_len", type=int, default=40,
+                        help="Maximum sentence length for position embedding")
+
+    # options for the distributional signatures
+    parser.add_argument("--meta_idf", action="store_true", default=False,
+                        help="use idf")
+    parser.add_argument("--meta_iwf", action="store_true", default=False,
+                        help="use iwf")
+    parser.add_argument("--meta_w_target", action="store_true", default=False,
+                        help="use target importance score")
+    parser.add_argument("--meta_w_target_lam", type=float, default=1,
+                        help="lambda for computing w_target")
+    parser.add_argument("--meta_target_entropy", action="store_true", default=False,
+                        help="use inverse entropy to model task-specific importance")
+    parser.add_argument("--meta_ebd", action="store_true", default=False,
+                        help="use word embedding into the meta model "
+                        "(showing that revealing word identity harm performance)")
+
+    # training options
+    parser.add_argument("--seed", type=int, default=123, help="seed")
+    parser.add_argument("--dropout", type=float, default=0.1, help="drop rate")
+    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
+    parser.add_argument("--patience", type=int, default=10, help="patience")
+    parser.add_argument("--clip_grad", type=float, default=None,
+                        help="gradient clipping")
+    parser.add_argument("--cuda", type=int, default=0,
+                        help="cuda device, -1 for cpu")
+    parser.add_argument("--mode", type=str, default="train",
+                        help=("Running mode."
+                              "Options: [train, test, finetune]"
+                              "[Default: test]"))
+    parser.add_argument("--save", action="store_true", default=False,
+                        help="train the model")
+    parser.add_argument("--notqdm", action="store_true", default=False,
+                        help="disable tqdm")
+    parser.add_argument("--result_path", type=str, default="results.json")
+    parser.add_argument("--snapshot", type=str, default="",
+                        help="path to the pretraiend weights")
+
+    parser.add_argument("--head_num", type=int,default=6)
+    parser.add_argument("--few_shot_num", type=int,default=1)
+
+
+    return parser.parse_args()
+
+
+def set_seed(seed):
+    """
+        Setting random seeds
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+
+
+def main():
+  
+    args = parse_args()
+
+    set_seed(args.seed)
+
+    # load data
+    train_data, val_data, test_data, vocab = loader.load_dataset(args)
+
+    # initialize model
+    model = {}
+    model["clf"] = clf.get_classifier(vocab, args)
+
+    if args.mode == "train":
+        # train model on train_data, early stopping based on val_data
+        train_utils.train(train_data, val_data, model, args)
+
+    test_acc, test_std = train_utils.test(test_data, model, args, args.test_episodes)
+
+    if args.result_path:
+        directory = args.result_path[:args.result_path.rfind("/")]
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+        result = {
+            "test_acc": test_acc,
+            "test_std": test_std,
+        }
+
+        name = ['classifier', 'embedding', 'way', 'shot', 'query', 'data_path', 'dataset', 'lr', 'mode', 'pretrain_name']
+        for attr, value in sorted(args.__dict__.items()):
+            if attr in name:
+                result[attr] = value
+
+        with open(args.result_path, "a", encoding='UTF-8') as f:
+            result = json.dumps(result, ensure_ascii=False)
+            f.writelines(result + '\n')
+
+
+
+if __name__ == "__main__":
+    main()
 
