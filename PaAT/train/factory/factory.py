b792f9f36e793afe157f568ffa0b8ec96bfc239b
YS19999 <117980405+YS19999@users.noreply.github.com>
2023-10-25 10:06:26 +0800

    Update factory.py

--- PaAT/train/factory/factory.py
+++ PaAT/train/factory/factory.py
@@ -1,19 +1,11 @@
-import train.maml as maml
+
 import train.regular as regular
 
 
 def train(train_data, val_data, model, args):
-   if args.maml:
-        return maml.train(train_data, val_data, model, args)
-   else:
-        return regular.train(train_data, val_data, model, args)
+   return regular.train(train_data, val_data, model, args)
 
 
 def test(test_data, model, args, verbose=True):
 
-    if args.maml:
-        return maml.test(test_data, model, args, verbose)
-    elif args.mode == 'finetune':
-        return finetune.test(test_data, model, args, verbose)
-    else:
-        return regular.test(test_data, model, args, verbose)
+    return regular.test(test_data, model, args, verbose)
